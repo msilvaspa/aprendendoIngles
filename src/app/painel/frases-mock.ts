237d85b5fd352182fb32a17b3dd48287e6e59924
Mateus Alves <msilva.spa@hotmail.com>
2018-03-25 14:29:50 -0300

    implementando logica

--- src/app/painel/frases-mock.ts
+++ src/app/painel/frases-mock.ts
@@ -1,6 +1,6 @@
 import { Frase } from '../shared/frase.model'
 
-const FRASES: Frase[] = [
+export const FRASES: Frase[] = [
     { fraseEng: 'I like to learn', frasePtBr: 'Eu gosto de aprender' },
     { fraseEng: 'I watch TV', frasePtBr: 'Eu assisto TV' },
     { fraseEng: 'How are you?', frasePtBr: 'Como vai você?' },
--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Frase } from '../shared/frase.model'
+import { FRASES } from './frases-mock'
 
 @Component({
   selector: 'app-painel',
@@ -7,9 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PainelComponent implements OnInit {
 
-  constructor() { }
+  public instrucao: string = 'Traduza a frase:';
+  public frases: Frase[] = FRASES;
+  public resposta: string;
+
+  public rodada: number = 0;
+  public rodadaFrase: Frase;
+
+  constructor() {
+    this.rodadaFrase = this.frases[this.rodada]
+    console.log(this.rodadaFrase)
+   }
 
   ngOnInit() {
+  }
+
+  public atualizaResposta(resposta: Event): void {
+    this.resposta = ((<HTMLInputElement>resposta.target).value);
+    console.log(this.resposta);
+  }
+
+  public verificarResposta(): void {
+    console.log('Verificar resposta: ', this.resposta);
   }
 
 }
