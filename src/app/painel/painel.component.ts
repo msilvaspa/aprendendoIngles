97c8fcbd47bbbee3826fa55abd8c191b059539f9
Mateus Alves <msilva.spa@hotmail.com>
2018-03-25 16:15:24 -0300

    logica de rodadas e progresso concluído

--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -11,26 +11,45 @@ export class PainelComponent implements OnInit {
 
   public instrucao: string = 'Traduza a frase:';
   public frases: Frase[] = FRASES;
-  public resposta: string;
+  public resposta: string = '';
 
   public rodada: number = 0;
   public rodadaFrase: Frase;
 
+  public progresso: number = 0;
+
   constructor() {
-    this.rodadaFrase = this.frases[this.rodada]
-    console.log(this.rodadaFrase)
-   }
+    this.atualizaRodada()
+  }
 
   ngOnInit() {
   }
 
   public atualizaResposta(resposta: Event): void {
     this.resposta = ((<HTMLInputElement>resposta.target).value);
-    console.log(this.resposta);
+    // console.log(this.resposta);
   }
 
   public verificarResposta(): void {
-    console.log('Verificar resposta: ', this.resposta);
+
+    if (this.rodadaFrase.frasePtBr == this.resposta) {
+      // alert('A tradução está correta');
+      this.rodada++;
+
+      this.progresso = this.progresso + (100 / this.frases.length);
+      // console.log(this.progresso);
+      this.atualizaRodada();
+    } else {
+      alert('A tradução está errada');
+    }
+
+  }
+
+  public atualizaRodada(): void {
+    //define a frase da rodada 
+    this.rodadaFrase = this.frases[this.rodada];
+    //limpa a resposta
+    this.resposta = '';
   }
 
 }
