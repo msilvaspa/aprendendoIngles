9fb2c6b751dd9143671b68c8ac8d5e1d6504a37b
Mateus Alves <msilva.spa@hotmail.com>
2018-03-24 23:07:21 -0300

    utilizando dataBind e string interpolation

--- src/app/topo/topo.component.ts
+++ src/app/topo/topo.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TopoComponent implements OnInit {
 
+  public titulo: string = 'Aprendendo inglês'
   constructor() { }
 
   ngOnInit() {
